90ea0b18e91721d9b514557737ec0819379c3394
Erik Kjosavik <erik.kjosavik@gmail.com>
2017-09-12 16:16:19 +0200

    Added test to implementation. This time with the correct methods

--- crypto.hpp
+++ crypto.hpp
@@ -41,7 +41,7 @@ public:
   static std::string sha256(const std::string &input) {
     std::string hash;
     hash.resize(256 / 8);
-    SHA1((const unsigned char *)input.data(), input.size(), (unsigned char *)hash.data());
+    SHA256((const unsigned char *)input.data(), input.size(), (unsigned char *)hash.data());
     return hash;
   }
 
@@ -49,7 +49,7 @@ public:
   static std::string sha512(const std::string &input) {
     std::string hash;
     hash.resize(512 / 8);
-    SHA1((const unsigned char *)input.data(), input.size(), (unsigned char *)hash.data());
+    SHA512((const unsigned char *)input.data(), input.size(), (unsigned char *)hash.data());
     return hash;
   }
 
